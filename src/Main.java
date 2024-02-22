576181e261e595950bdd9f49005d082838703b01
kila1 <kila14081996@gmail.com>
2024-02-23 02:17:13 +0300

    Домашняя работа №5. Задание 1

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Task 1
+        byte clientOs = 0;
+        if (clientOs == 1) {
+            System.out.println("Установите версию приложения для IOS по ссылке");
+        } else if ( clientOs == 0) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Ошибка");
+        }
+
+        //Task 2
+
+    }
+}
