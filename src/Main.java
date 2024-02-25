1b798693eae7cea78729f7c869f58fc91b31eae3
kila1 <kila14081996@gmail.com>
2024-02-25 21:51:32 +0300

    Домашняя работа №5. Задание 2

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,26 @@ public class Main {
         byte clientOs = 0;
         if (clientOs == 1) {
             System.out.println("Установите версию приложения для IOS по ссылке");
-        } else if ( clientOs == 0) {
+        } else if (clientOs == 0) {
             System.out.println("Установите версию приложения для Android по ссылке");
         } else {
             System.out.println("Ошибка");
         }
 
         //Task 2
-
+        clientOs = 0;
+        int clientDeviceYear = 2000;
+        if (clientOs == 1) {
+            System.out.println("Установите версию приложения для IOS по ссылке");
+            if (clientOs == 1 && clientDeviceYear <= 2015) {
+                System.out.println("Установите облегченную версию приложения для IOS по ссылке");
+            }
+        }
+        if (clientOs == 0) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+            if (clientOs == 0 && clientDeviceYear <= 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+        }
     }
 }
